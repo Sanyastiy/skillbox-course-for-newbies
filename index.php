faf4379bd93fa2118f6a19722edb1d7f179c7538
Alexander Svinarenko <akselsv@mail.ru>
2020-05-29 17:01:01 +0300

    домашнее задание первой части курса

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>skill box course</title>
+    
+</head>
+<body>
+
+<?php include("header.php"); ?>
+
+<?php include("swining-cat-code.php"); ?>
+
+</body>
+</html>   
