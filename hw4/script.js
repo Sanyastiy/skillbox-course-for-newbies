e0523d1674ee8440ded7d01bed60aa5c8cec291e
Alexander Svinarenko <akselsv@mail.ru>
2020-07-17 16:04:27 +0300

    hw4.4, 4.5 done

--- hw4/script.js
+++ hw4/script.js
@@ -1,9 +1,11 @@
 $(function () {
-    function disableScroll(){
-        $('html, body').on('mousewheel',function () {
+    function disableScroll() {
+        $('html, body').on('mousewheel', function () {
             return false;
         })
     }
+
+    disableScroll();
     //Добавить дело
     $('#submit')
         .click(function () {
@@ -15,7 +17,7 @@ $(function () {
                     + '<div class="business_head"><h3>'
                     + $("#name").val()
                     + ' <i class="close">&times;</i>'
-                    + ' <i class="expand">&#9650;</i>'
+                    + ' <i class="reduce">&#9650;</i>'
                     + '</h3></div><div class="business_body">'
                     + $("#description").val()
                     + '</div>'
@@ -28,14 +30,37 @@ $(function () {
             //Крестик на карточке
             $('.close').click(function () {
                 //Удаление карточки по классу с анимацией
-                $(this).closest('.business_card').hide().fadeOut(1100).remove();
+                $(this).closest('.business_card').fadeOut(1100).remove();
                 //Если карточек больше нет, то показать надпись Список пуст
-                if($('.close').length==0){
+                if ($('.close').length == 0) {
                     $('#list_empty').show().fadeIn(1100);
                 }
             })
-            $('.expand').click(function () {
-                $(this).closest('.business_body').hide().fadeOut(1100);
+
+            //скрытие описания карточки
+            //находим стрелку сворачивания
+            $('.reduce').click(function () {
+                //при клике попадаем сюда, и каждый клик будет всегда приходить сюда и не выйдет за пределы этой функции
+                //по ифу определяем что будет делать "кнопка"
+                if ($(this).hasClass("reduce")) {
+                    //зашли в сворачивание, меняем класс кнопки
+                    $(this).removeClass('reduce').addClass('expand');
+                    //сворачиваем блок
+                    $(this).closest('.business_head').next('.business_body').hide().fadeOut(1100);
+                    //меняем символ
+                    $(this).html('&#9660;');
+                } else
+                    //по ифу определяем что будет делать "кнопка"
+                if ($(this).hasClass("expand")) {
+                    //зашли в сворачивание, меняем класс кнопки
+                    $(this).removeClass('expand').addClass('reduce');
+                    //разворачиваем блок
+                    $(this).closest('.business_head').next('.business_body').show().fadeIn(1100);
+                    //меняем символ
+                    $(this).html('&#9650;');
+                }
             })
+
         })
+
 });
