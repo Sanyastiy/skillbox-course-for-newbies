3dcf6a9c483fe6f26b747d71cbc3a7d072ed2d91
Alexander Svinarenko <akselsv@mail.ru>
2020-08-18 15:57:33 +0300

    corrections in 4 about naming; in main about separate styles file added 5.1, 5.2

--- hw4/script.js
+++ hw4/script.js
@@ -9,33 +9,36 @@ $(function () {
     //Добавить дело
     $('#submit')
         .click(function () {
-            //убрать надпись Список пуст
-            $('#list_empty').hide().fadeOut(1100);
-            //Создание карточки
-            var newElement =
-                $('<div class="business_card"> '
-                    + '<div class="business_head"><h3>'
-                    + $("#name").val()
-                    + ' <i class="close">&times;</i>'
-                    + ' <i class="reduce">&#9650;</i>'
-                    + '</h3></div><div class="business_body">'
-                    + $("#description").val()
-                    + '</div>'
-                    + '</div>');
-            //Добавить картчоку
-            $(newElement).prependTo($('#main_business')).hide().fadeIn(1100);
-            //Очистка полей
-            $('#name').val('');
-            $('#description').val('');
-            //Крестик на карточке
-            $('.close').click(function () {
-                //Удаление карточки по классу с анимацией
-                $(this).closest('.business_card').fadeOut(1100).remove();
-                //Если карточек больше нет, то показать надпись Список пуст
-                if ($('.close').length == 0) {
-                    $('#list_empty').show().fadeIn(1100);
-                }
-            })
+            // noinspection JSJQueryEfficiency
+            if (($('#name').val !== '') && ($('#description').val !== '')) {
+                //убрать надпись Список пуст
+                $('#list_empty').hide().fadeOut(1100);
+                //Создание карточки
+                var newElement =
+                    $('<div class="card"> '
+                        + '<div class="card__head"><h3>'
+                        + $("#name").val()
+                        + ' <i class="close">&times;</i>'
+                        + ' <i class="reduce">&#9650;</i>'
+                        + '</h3></div><div class="card__body">'
+                        + $("#description").val()
+                        + '</div>'
+                        + '</div>');
+                //Добавить картчоку
+                $(newElement).prependTo($('#main_business')).hide().fadeIn(1100);
+                //Очистка полей
+                $('#name').val('');
+                $('#description').val('');
+                //Крестик на карточке
+                $('.close').click(function () {
+                    //Удаление карточки по классу с анимацией
+                    $(this).closest('.card').fadeOut(1100).remove();
+                    //Если карточек больше нет, то показать надпись Список пуст
+                    if ($('.close').length === 0) {
+                        $('#list_empty').show().fadeIn(1100);
+                    }
+                })
+            }
 
             //скрытие описания карточки
             //находим стрелку сворачивания
@@ -46,7 +49,7 @@ $(function () {
                     //зашли в сворачивание, меняем класс кнопки
                     $(this).removeClass('reduce').addClass('expand');
                     //сворачиваем блок
-                    $(this).closest('.business_head').next('.business_body').hide().fadeOut(1100);
+                    $(this).closest('.card__head').next('.card__body').hide().fadeOut(1100);
                     //меняем символ
                     $(this).html('&#9660;');
                 } else
@@ -55,7 +58,7 @@ $(function () {
                     //зашли в сворачивание, меняем класс кнопки
                     $(this).removeClass('expand').addClass('reduce');
                     //разворачиваем блок
-                    $(this).closest('.business_head').next('.business_body').show().fadeIn(1100);
+                    $(this).closest('.card__head').next('.card__body').show().fadeIn(1100);
                     //меняем символ
                     $(this).html('&#9650;');
                 }
